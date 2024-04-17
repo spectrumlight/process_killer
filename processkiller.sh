02104a4e2c259ac9583af17af4de5227fb7cfd2a
spectrumlight <149823813+spectrumlight@users.noreply.github.com>
2024-04-18 00:10:04 +0300

    processkiller.sh

--- processkiller.sh
+++ processkiller.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 # Define the process name
-process_name1=".initd"
+process_name="your_process_name"
 
 # Check if the process is running
-if pgrep -x "$process_name1" > /dev/null
-then
-    echo "Process $process_name1 is running."
+if pgrep "$process_name" >/dev/null 2>&1; then
+    # If the process is running, kill it
+    pkill "$process_name"
+    echo "Process '$process_name' has been killed."
 else
-    echo "Process $process_name1 is not running. Starting it..."
-    # Start the process
-    /var/spool/"$process_name1" &
+    echo "Process '$process_name' is not running."
 fi
-
-
 
